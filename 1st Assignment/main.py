a9643c57ae9234b3ae4b0841e22321226a7f60e5
Diogenis <dio.tsolakou@hotmail.com>
2021-04-20 04:14:47 +0300

    Getting there

--- 1st Assignment/main.py
+++ 1st Assignment/main.py
@@ -1,20 +1,12 @@
-import math
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # globals for now
 w1 = []  # Mx(D+1) matrix, j line has vector wj
 w2 = []  # Kx(M+1) matrix, k line has vector wk
 x = []  # input data of (D+1)-dimensions
 pick = 0
-
-
-def main():
-    stochastic_gradient_ascent()
-
-
-if __name__ == "__main__":
-    main()
 
 
 def stochastic_gradient_ascent():
@@ -28,12 +20,14 @@ def gradcheck():   # function to check partial derivative
     pass
 
 
-def cost(w, K, N, lamda):
+def cost(w, X, t, lamda):
     cost_of_w = 0
+    N, D = X.shape
+    K = t.shape[1]
 
     for n in range(N):
         for k in range(K):
-            cost_of_w += (t * math.log10(y(K, k, n))) - (lamda/2) * math.pow(np.linalg.norm(w), 2)  # t to be declared later
+            cost_of_w += (t[n][k] * np.log(y(K, k, n))) - (lamda/2) * np.power(w, 2)  # t to be declared later
 
     return cost_of_w
 
@@ -41,9 +35,9 @@ def cost(w, K, N, lamda):
 def y(K, index_k, index_n):
     denominator = 0
     for i in range(K):
-        denominator += math.exp(np.transpose(w2[i]) * z(index_n, 0))  # j is 0 for now
+        denominator += np.exp(np.transpose(w2[i]) * z(index_n, 0))  # j is 0 for now
 
-    return math.exp(np.transpose(w2[index_k]) * z(index_n, 0)) / denominator  # j is 0 for now
+    return np.exp(np.transpose(w2[index_k]) * z(index_n, 0)) / denominator  # j is 0 for now
 
 
 def z(n, j):
@@ -52,11 +46,15 @@ def z(n, j):
 
 def activation_func(a):
     if pick == 0:
-        return math.log10(1 + math.exp(a))
+        return np.log(1 + np.exp(a))
     elif pick == 1:
-        return (math.exp(a) - math.exp(-a)) / math.exp(a) + math.exp(-a)
+        return (np.exp(a) - np.exp(-a)) / np.exp(a) + np.exp(-a)
     else:
-        return math.cos(a)
+        return np.cos(a)
+
+
+def weight_init(f_in, f_out):
+    return np.random.uniform(-np.sqrt(6/(f_in + f_out)), np.sqrt(6/(f_in + f_out)))
 
 
 def load_data():
@@ -70,7 +68,6 @@ def load_data():
     y_test: the matrix consisting of one
                         hot vectors on each row(ground truth for testing)
     """
-
     # load the train files
     train_data, y_train = load_file("train")
 
@@ -85,7 +82,8 @@ def load_file(file):
     df = None
     
     for i in range(10):
-        tmp = pd.read_csv('data/mnist/%s%d.txt' % file % i, header=None, sep=" ")
+        print("Now loading file : data/mnist/{0}{1}.txt".format(file, i))
+        tmp = pd.read_csv("data/mnist/{0}{1}.txt".format(file, i), header=None, sep=" ")
         # build labels - one hot vector
         hot_vector = [1 if j == i else 0 for j in range(0, 10)]
 
@@ -101,3 +99,30 @@ def load_file(file):
     array = np.array(array)
     
     return data, array
+
+
+def view_dataset(train_data):
+    n = 100
+    sqrt_n = int(n**0.5)
+    samples = np.random.randint(train_data.shape[0], size=n)
+
+    plt.figure(figsize=(11, 11))
+
+    counter = 0
+    for i in samples:
+        counter += 1
+        plt.subplot(sqrt_n, sqrt_n, counter)
+        plt.subplot(sqrt_n, sqrt_n, counter).axis('off')
+        plt.imshow(train_data[i].reshape(28, 28), cmap='gray')
+
+    plt.show()
+
+
+def main():
+    train_data, test_data, y_train, y_test = load_data()
+    view_dataset(train_data)
+    #stochastic_gradient_ascent()
+
+
+if __name__ == "__main__":
+    main()
