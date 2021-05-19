8a8c663f2ccffb20879447b1c11d720315d0e22d
Diogenis <dio.tsolakou@hotmail.com>
2021-05-19 12:54:04 +0300

    2nd Assignment initial commit

---
+++ 2nd Assignment/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def error_func():
+    return
+
+
+def gaussian_distr():
+    return
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
