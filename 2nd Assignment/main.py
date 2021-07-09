653878bdd85f4fd8d8f17224a0919430e6110f3d
Diogenis <dio.tsolakou@hotmail.com>
2021-07-09 23:03:32 +0300

    Progress I guess. Fixed some things implemented some more.

--- 2nd Assignment/main.py
+++ 2nd Assignment/main.py
@@ -1,12 +1,12 @@
 import random
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import imageio
+import scipy.stats
 
 
 def init_params(X, k):
-    #probabilities =
+    probabilities = np.full(shape=k, fill_value=1/k)
     means = np.asarray(random.sample(list(X), k))
     sigma = [np.cov(X.T) * np.identity(X.shape[1]) for _ in range(k)]
     print("means shape : ", means.shape)
@@ -14,12 +14,14 @@ def init_params(X, k):
     print("sigma shape : ", len(sigma))
     #print("sigma content : ", sigma)
 
+    return probabilities, means, sigma
+
 
 def load_img(file):
     original_img = imageio.imread(file)
     imgplot = plt.imshow(original_img)
     plt.show()
-    print("original shape : ", original_img.shape)
+    #print("original shape : ", original_img.shape)
     #print("original content : ", original_img)
     return original_img
 
@@ -33,34 +35,63 @@ def flatten_img(file):
 
 
 def from_2d_to_3d(file, x, y):
-    file = (file * 255).astype(np.uint8)
+    file = np.array(file, dtype=np.uint)
     img_3d = file.reshape(x, y, 3)
-    print("reconstructed img shape : ", img_3d.shape)
+    #print("reconstructed img shape : ", img_3d.shape)
     #print("reconstructed img content : ", img_3d)
     imgplot = plt.imshow(img_3d)
     plt.show()
     return img_3d
 
 
-def error_func():
-    return
+def probability(x, pis, means, sigma):
+    gmm = scipy.stats.multivariate_normal.pdf(x, means, sigma)
+    enumerator = pis * gmm
+    denominator = enumerator.sum(axis=1)[:, np.newaxis]
+    gamma = enumerator / denominator
+    return gamma
 
 
-def gaussian_distr():
-    return
+def error_func(original_img, reconstructed_img):
+    if original_img.shape != reconstructed_img.shape:
+        return -1
+
+    original_img_norm = np.sum(np.square(original_img), axis=0, keepdims=True)
+    original_img_norm = np.sum(original_img_norm, axis=1, keepdims=True)
+    original_img_norm = np.sqrt(original_img_norm)
+    original_img_norm = np.array(original_img_norm)
+
+    reconstructed_img_norm = np.sum(np.square(reconstructed_img), axis=0, keepdims=True)
+    reconstructed_img_norm = np.sum(reconstructed_img_norm, axis=1, keepdims=True)
+    reconstructed_img_norm = np.sqrt(reconstructed_img_norm)
+    reconstructed_img_norm = np.array(reconstructed_img_norm)
+
+    error = np.square(original_img_norm - reconstructed_img_norm) / N
+    print("Error is : ", error)
 
 
-def ml_train():
+def ml_train(x, pis, means, sigma, iterations, k):
+    for _ in iterations:
+        # E step
+        gamma = probability(x, pis, means, sigma)
+        pis = gamma.mean(axis=0)
+
+        # M step
+        for i in k:
+            gamma =
     return
 
 
 def main():
     k = int(input("Give the desired k : "))
+    iterations = int(input("Give the desired amount of iterations : "))
     original_img = load_img("im.jpg")
     img_2d = flatten_img(original_img)
-    init_params(img_2d, k)
+    pis, means, sigma = init_params(img_2d, k)
+    ml_train(img_2d, pis, means, sigma, iterations, k)
 
-    #from_2d_to_3d(img_2d, original_img.shape[0], original_img.shape[1])
+    reconstructed_img = from_2d_to_3d(img_2d, original_img.shape[0], original_img.shape[1])
+    error_func(original_img, reconstructed_img)
 
 
 if __name__ == "__main__":
